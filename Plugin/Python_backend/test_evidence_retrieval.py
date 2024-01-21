94ace9453e7e3a427edeb47ed9efd702c8ee8396
Ella <ellahorne02@gmail.com>
2024-01-21 20:52:50 +0000

    Think links are being fetched from the queried page. Unsure

--- Plugin/Python_backend/test_evidence_retrieval.py
+++ Plugin/Python_backend/test_evidence_retrieval.py
@@ -5,11 +5,12 @@ from selenium.webdriver.chrome.service import Service
 from scrapy.selector import Selector
 import time
 from scrapy.crawler import CrawlerProcess
+from bs4 import BeautifulSoup
 
 class BBCSpider(scrapy.Spider):
     name = 'bbc'
     allowed_domains = ['bbc.co.uk']
-    # start_urls = ['https://www.cnn.com']
+    # start_urls = ['https://www.cnn.com'] # something to add to
 
     # def __init__(self):
 
@@ -20,20 +21,37 @@ class BBCSpider(scrapy.Spider):
         
 
     def parse_search_results(self, response):
-        article_links = response.css('a::attr(href)').extract() # css might need to change
+        # response is the response of the request where this function is called
+        # is a Response object?
+        # article_links = response.css('a::attr(href)').extract()
+        # print("O", response)
+        soup = BeautifulSoup(response.body, 'html.parser')
+        # need to extract non header and footer stuff
+        href_links = [a.get('href') for a in soup.find_all('a', href=True)]
+        print(href_links)
+
+        article_links = response.css('a::attr(href)').getall()
+
+
         # so this doesn't work. need to delve into different divs to find relevant links
         
         # currently its getting nothing for the first 2 things and only doing the parse article bit
-        for link in article_links:
-            yield scrapy.Request(url=link, callback=self.parse_article)
+
+        # once able to access these links, need to be able to go through those links?
+        for link in href_links:
+            try:
+                yield scrapy.Request(url=link, callback=self.parse_article) # in theory this should go through found links and parse those articles
+            except:
+                pass
 
     def parse_article(self, response):
         title = response.css('h1::text').get()
-        content = response.css('body').extract()
+        # content = response.css('body').extract()
+        # content = response.css('p::text').getall() # CHANGE THIS
         # content is returning nothing
         yield {
             'title': title,
-            'content': ' '.join(content),
+            # 'content': ' '.join(content),
             'url': response.url
         }
 
