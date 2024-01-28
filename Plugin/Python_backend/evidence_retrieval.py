c9cca19a45abdcb5b371eec42accd1021091bafc
Ella <ellahorne02@gmail.com>
2024-01-28 14:58:50 +0000

    Have written some more for evidence retrieval and extracting text from links found. Have written plan for how to next proceed with links and getting claims from text

--- Plugin/Python_backend/evidence_retrieval.py
+++ Plugin/Python_backend/evidence_retrieval.py
@@ -12,6 +12,8 @@ class evidence_retrieval_spider(scrapy.Spider):
     start_urls = ['https://duckduckgo.com'] 
     # have a whitelist of domains to search through
 
+    evidence_and_articles_text = {}
+
     # method to start the scraping
     def start_requests(self):
         search_term = 'Thousands stranded at New Year as Eurostar cancelled'
@@ -26,7 +28,7 @@ class evidence_retrieval_spider(scrapy.Spider):
         # href_links = [a.get('href') for a in soup.find_all('a', href=True)]
 
         links = soup.find_all('a', class_="result__url")
-        print("\n\n", links, "\n\n")
+        # print("\n\n", links, "\n\n") # testing
         # need to parse the list of tags to get the actual link
         new_links = self.parse_links(links)
 
@@ -38,12 +40,34 @@ class evidence_retrieval_spider(scrapy.Spider):
         #     except:
         #         pass
 
+        # iterate through each link, call a function to parse the article
+        for link in new_links:
+            try:
+                # store link in dict - will be link:claims pairing
+                yield scrapy.Request(url=link, callback=self.parse_article)
+            except:
+                pass
+
     def get_link(self, link):
         return str(link).split("</a>")[0].split(">")[-1].strip()
 
     def parse_links(self, links):
         # parse the list of links to get the actual link
         return [self.get_link(link) for link in links]
+    
+    # parse the article to get the text
+    def parse_article(self, response):
+        # need article tag
+        soup = BeautifulSoup(response.body, 'html.parser')
+        article = soup.find('article')
+        if article:
+            paragraphs = article.find_all('p')
+            article_text = ''
+            for paragraph in paragraphs:
+                article_text += paragraph.text
+
+            # now we have the text, need to pick claims out of it to compare
+        pass
 
 process = CrawlerProcess(settings={
     'FEED_FORMAT': 'json',
