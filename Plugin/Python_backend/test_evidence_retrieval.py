fbb62c5c592c2139c11ca3811c4726f066416ed9
Ella <ellahorne02@gmail.com>
2024-02-07 19:26:05 +0000

    Tried implementing a non spider version of evidence_retrieval but getting an error when doing that type of request

--- Plugin/Python_backend/test_evidence_retrieval.py
+++ Plugin/Python_backend/test_evidence_retrieval.py
@@ -1,72 +1,74 @@
-import scrapy
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from scrapy.selector import Selector
-import time
-from scrapy.crawler import CrawlerProcess
+import requests
 from bs4 import BeautifulSoup
 
-class BBCSpider(scrapy.Spider):
-    name = 'bbc'
-    allowed_domains = ['bbc.co.uk']
-    start_urls = ['https://www.bbc.co.uk'] # something to add to
+# need a white list of domains that can accept
 
-    # def __init__(self):
+
+# just need to do requests.get(<url>) to get html of web page
+
+# get search_term passed in
+# search_url = f'https://duckduckgo.com/html/?q={self.search_term}&type=article'
+
+class evidence_retrieval:
+    def __init__(self, search_term):
+        self.search_term = search_term
+        self.evidence_and_articles_text = {}
 
     def start_requests(self):
-        search_term = 'Thousands stranded at New Year as Eurostar cancelled'
-        search_url = f'https://duckduckgo.com/html/?q={search_term}&type=article'
-        # search_url = f'https://www.bbc.co.uk/search?q={search_term}&type=article'
-        # this type of querying doesn't work for the independent, for example
-        request = scrapy.Request(url=search_url, callback=self.parse_search_results)
-        # SHOULDN'T USE MY LOCAL TOKEN
-        # request.cookies()
-        yield request # set the cookies token somehow
-        
+        # search_term = self.search_term
+        search_url = f'https://duckduckgo.com/html/?q={self.search_term}&type=article'
+        request = requests.get(search_url)
+        return request.text
 
+    # take the response and extract the html
     def parse_search_results(self, response):
-        # response is the response of the request where this function is called
-        # is a Response object?
-        # article_links = response.css('a::attr(href)').extract()
-        print("PARSE_SEARCH_RESULTS")
         soup = BeautifulSoup(response.body, 'html.parser')
-        # need to extract non header and footer stuff
-        print(soup.prettify())
-        href_links = [a.get('href') for a in soup.find_all('a', href=True)]
-        # some form of regex 
-        print("HREF ", href_links)
+        # want to look for result_url class - duck duck go specific - holds full link
+        # href_links = [a.get('href') for a in soup.find_all('a', href=True)]
 
-        article_links = response.css('a::attr(href)').getall()
+        links = soup.find_all('a', class_="result__url")
+        # print("\n\n", links, "\n\n") # testing
+        # need to parse the list of tags to get the actual link
+        new_links = self.parse_links(links)
 
+        print(new_links)
 
-        # so this doesn't work. need to delve into different divs to find relevant links
-        
-        # currently its getting nothing for the first 2 things and only doing the parse article bit
+        # for link in href_links:
+        #     try:
+        #         yield scrapy.Request(url=link, callback=self.parse_article)
+        #     except:
+        #         pass
 
-        # once able to access these links, need to be able to go through those links?
-        for link in href_links:
-            try:
-                yield scrapy.Request(url=link, callback=self.parse_article) # in theory this should go through found links and parse those articles
-            except:
-                pass
+        # iterate through each link, call a function to parse the article
+        for link in new_links:
+            yield scrapy.Request(url=link, callback=self.parse_article)
+            # try:
+            #     # store link in dict - will be link:claims pairing
+            #     yield scrapy.Request(url=link, callback=self.parse_article)
+            # except:
+            #     pass
 
-    def parse_article(self, response): # this isn't running
-        print("Response", response)
-        title = response.css('h1::text').get()
-        # content = response.css('body::text').get()
-        # content = response.css('p::text').getall() # CHANGE THIS
-        # content is returning nothing
-        yield {
-            'title': title,
-            # 'content': ' '.join(content),
-            'url': response.url
-        }
+    def get_link(self, link):
+        return "https://" + str(link).split("</a>")[0].split(">")[-1].strip()
 
-process = CrawlerProcess(settings={
-    'FEED_FORMAT': 'json',
-    'FEED_URI': 'output1.json'
-})
+    def parse_links(self, links):
+        # parse the list of links to get the actual link
+        return [self.get_link(link) for link in links]
+    
+    # parse the article to get the text
+    def parse_article(self, response):
+        print("article")
+        # need article tag
+        link = response.url
+        soup = BeautifulSoup(response.body, 'html.parser')
+        article = soup.find('article')
+        if article:
+            paragraphs = article.find_all('p')
+            article_text = ''
+            for paragraph in paragraphs:
+                article_text += paragraph.text
 
-process.crawl(BBCSpider)
-process.start()
+            # now we have the text, need to pick claims out of it to compare
+                
+object = evidence_retrieval("Thousands stranded at New Year as Eurostar cancelled")
+print(object.start_requests())
